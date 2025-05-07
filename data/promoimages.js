4d44b36e8bc4d65f998629fdb6ff66290c4e0c97
Carlos Andres <sandovalcarlos@uninorte.edu.co>
2025-05-06 22:24:55 -0500

    Remove unused data files and clean up db.js module exports

--- data/promoimages.js
+++
@@ -1,6 +0,0 @@
-const promoImages = [
-    { id: 1, image: 'https://web-fe-html-css-prj2-apollo.onrender.com/assets/promo1-Diftpkmv.png', imageResponsive: 'https://web-fe-html-css-prj2-apollo.onrender.com/assets/promo1-res-CvWBFSl3.png', alt: 'Producto con descuento 1' },
-    { id: 2, image: 'https://web-fe-html-css-prj2-apollo.onrender.com/assets/promo2-DoJFuR2h.png', imageResponsive: 'https://web-fe-html-css-prj2-apollo.onrender.com/assets/promo2-res-BhJhHeSi.png', alt: 'Producto con descuento 2' },
-];
-
-module.exports = promoImages
