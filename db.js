bfa737557fa4e5364d202b87c233e959a5342a35
Carlos Andres <sandovalcarlos@uninorte.edu.co>
2025-05-08 00:02:09 -0500

    Add filters, featured products, and promotional images data

--- db.js
+++ db.js
@@ -1,8 +1,17 @@
+
+const featuredProducts = require("./data/featuredproducts.data");
 const pdp = require("./data/Pdp.data");
 const plp = require("./data/Plp.data");
+const filters = require("./data/Filters.data");
+const promoImages = require("./data/promoimages");
+
 module.exports = () => {
   return {
     plp,
     pdp,
+    filters,
+    featuredProducts,
+    promoImages,
+
   };
 };
