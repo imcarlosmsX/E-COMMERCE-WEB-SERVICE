a71bcc1683d6f77034897b8abd14baa75e4193af
Carlos Andres <sandovalcarlos@uninorte.edu.co>
2025-05-13 17:45:27 -0500

    Add new products: Mochila Wayuu Amarilla, Mochila Wayuu Rosa, and Sombrero Vueltiao with updated details and discounts

--- data/Pdp.data.js
+++ data/Pdp.data.js
@@ -43,6 +43,51 @@ const products = [
             { feature: "Color", value: "Verde" },
             { feature: "Material", value: "Algodón" }
         ],
+    },
+    {
+        id: 90,
+        optionId: 1,
+        image: 'https://i.ibb.co/WN1vrMnW/35049-venta-ig-labs-album5-04-1-n.png',
+        name: 'Mochila Wayuu Amarilla',
+        rating: 4,
+        description: 'Amarilla',
+        normalPrice: 340000,
+        discountedPrice: 306000,
+        specifications: [
+            { feature: "Tamaño", value: "Grande" },
+            { feature: "Color", value: "Amarillo" },
+            { feature: "Material", value: "Algodón" }
+        ],
+    },
+    {
+        id: 91,
+        optionId: 1,
+        image: 'https://i.ibb.co/7W0Y2J4D/35049-venta-ig-labs-album5-04-2-n.png',
+        name: 'Mochila Wayuu Rosa',
+        rating: 4,
+        description: 'Rosa',
+        normalPrice: 100000,
+        discountedPrice: 80000,
+        specifications: [
+            { feature: "Tamaño", value: "Pequeña" },
+            { feature: "Color", value: "Rosa" },
+            { feature: "Material", value: "Algodón" }
+        ],
+    },
+    {
+        id: 92,
+        optionId: 2,
+        image: 'https://i.ibb.co/7W0Y2J4D/35049-venta-ig-labs-album5-04-2-n.png',
+        name: 'Sombrero Vueltiao',
+        rating: 4,
+        description: 'Original',
+        normalPrice: 350000,
+        discountedPrice: 280000,
+        specifications: [
+            { feature: "Tamaño", value: "Adulto" },
+            { feature: "Color", value: "Original" },
+            { feature: "Material", value: "Paja" }
+        ],
     }
 ];
 module.exports = products;
--- data/Plp.data.js
+++ data/Plp.data.js
@@ -47,6 +47,55 @@ const products_plp = [
             { feature: "Material", value: "Algodón" }
         ],
         discount: 10
+    },
+    {
+        id: 90,
+        optionId: 1,
+        image: 'https://i.ibb.co/WN1vrMnW/35049-venta-ig-labs-album5-04-1-n.png',
+        name: 'Mochila Wayuu Amarilla',
+        rating: 4,
+        description: 'Mochila Wayuu 100% Hecha a Mano',
+        normalPrice: 340000,
+        discountedPrice: 306000,
+        specifications: [
+            { feature: "Tamaño", value: "Grande" },
+            { feature: "Color", value: "Amarillo" },
+            { feature: "Material", value: "Algodón" }
+        ],
+        discount: 10
+    },
+    {
+        id: 91,
+        optionId: 1,
+        image: 'https://i.ibb.co/XZs1TfhW/3-C101-GSYQ-mochila-wayuu-rosada.png',
+        name: 'Mochila Wayuu Rosa',
+        rating: 4,
+        description: 'Mochila Wayuu 100% Hecha a Mano',
+        normalPrice: 100000,
+        discountedPrice: 80000,
+        specifications: [
+            { feature: "Tamaño", value: "Grande" },
+            { feature: "Color", value: "Rosa" },
+            { feature: "Material", value: "Algodón" }
+        ],
+        discount: 20
+    },
+    {
+        id: 92,
+        optionId: 2,
+        image: 'https://i.ibb.co/84GHZGDH/D-NQ-NP-649249-MCO74384299597-022024-O.png',
+        name: 'Sombrero Vueltiao',
+        rating: 4,
+        description: 'Sombrero Vueltiao 100% Hecha a Mano',
+        normalPrice: 350000,
+        discountedPrice: 280000,
+        specifications: [
+            { feature: "Tamaño", value: "Adulto" },
+            { feature: "Color", value: "Original" },
+            { feature: "Material", value: "Paja" }
+        ],
+        discount: 20
     }
+
 ];
 module.exports = products_plp;
