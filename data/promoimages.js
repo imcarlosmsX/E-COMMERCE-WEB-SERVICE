cb0581c41c2f5dfe23f7df415072c8055b9ee2a7
Carlos Andres <sandovalcarlos@uninorte.edu.co>
2025-05-16 23:50:44 -0500

    Update promotional image for Producto con descuento 1

--- data/promoimages.js
+++ data/promoimages.js
@@ -1,5 +1,5 @@
 const promoImages = [
-    { id: 1, image: 'https://web-fe-html-css-prj2-apollo.onrender.com/assets/promo1-Diftpkmv.png', imageResponsive: 'https://web-fe-html-css-prj2-apollo.onrender.com/assets/promo1-res-CvWBFSl3.png', alt: 'Producto con descuento 1' },
+    { id: 1, image: 'https://i.ibb.co/k6J6cyqM/Chat-GPT-Image-16-may-2025-23-47-02.png', imageResponsive: 'https://web-fe-html-css-prj2-apollo.onrender.com/assets/promo1-res-CvWBFSl3.png', alt: 'Producto con descuento 1' },
     { id: 2, image: 'https://web-fe-html-css-prj2-apollo.onrender.com/assets/promo2-DoJFuR2h.png', imageResponsive: 'https://web-fe-html-css-prj2-apollo.onrender.com/assets/promo2-res-BhJhHeSi.png', alt: 'Producto con descuento 2' },
 ];
 
