d8ac77acdf3a8968307a7dd1fd4e9b63ac82cdaf
Carlos Andres <sandovalcarlos@uninorte.edu.co>
2025-05-17 00:43:34 -0500

    Update image URLs for promotional products

--- data/promoimages.js
+++ data/promoimages.js
@@ -1,6 +1,6 @@
 const promoImages = [
-    { id: 1, image: 'https://web-fe-html-css-prj2-apollo.onrender.com/assets/promo1-Diftpkmv.png', imageResponsive: 'https://web-fe-html-css-prj2-apollo.onrender.com/assets/promo1-res-CvWBFSl3.png', alt: 'Producto con descuento 1' },
-    { id: 2, image: 'https://web-fe-html-css-prj2-apollo.onrender.com/assets/promo2-DoJFuR2h.png', imageResponsive: 'https://web-fe-html-css-prj2-apollo.onrender.com/assets/promo2-res-BhJhHeSi.png', alt: 'Producto con descuento 2' },
+    { id: 1, image: 'https://i.ibb.co/5Wsxf9nZ/Dise-o-sin-t-tulo.png', imageResponsive: 'https://web-fe-html-css-prj2-apollo.onrender.com/assets/promo1-res-CvWBFSl3.png', alt: 'Producto con descuento 1' },
+    { id: 2, image: 'https://i.ibb.co/DfFJSWpW/Dise-o-sin-t-tulo-1.png', imageResponsive: 'https://web-fe-html-css-prj2-apollo.onrender.com/assets/promo2-res-BhJhHeSi.png', alt: 'Producto con descuento 2' },
 ];
 
 module.exports = promoImages
